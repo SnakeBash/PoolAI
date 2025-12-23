40f45d3d0e3f6cb005f877f48b3d16f2a88b3b59
snakebash <snakebash@outlook.com>
2025-12-22 22:45:33 -0500

    Added basic stats to dataframe.
    Added more player info in the data and cleaned up some prints. Began experimenting with selecting a player from the console for the prototype.

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 from dotenv import load_dotenv
 from WebScraper import scraper as sc
 
@@ -6,8 +7,14 @@ def main():
     load_dotenv()
 
     api_key = os.getenv('API_KEY')
-    print(f"{api_key}")
-    sc()
+
+    df = sc()
+    print(df.head())
+
+    name = input("What player are you picking?")
+    print(f"{df.loc[df['Name'] == name]}")
+
+
 
 if __name__ == "__main__":
      main()
--- WebScraper.py
+++ WebScraper.py
@@ -6,29 +6,34 @@ def scraper():
     url = "https://www.hockey-reference.com/leagues/NHL_2026_skaters.html"
     data = requests.get(url)
 
-    print(f"{data}")
-
     soup = BeautifulSoup(data.text, 'html.parser')
 
     for table in soup.find_all('table'):
         print(table.get('class'))
 
-    tables = soup.find_all('table')
     table = soup.find('table', class_='stats_table')
 
-    df = pd.DataFrame(columns=['Name'])
+    df = pd.DataFrame(columns=['Name', 'Age', 'Team', 'Pos', 'GP', 'Goals', 'Assists', 'Points'])
 
     rows = []
 
     for row in table.tbody.find_all('tr'):
         # Find all data for each column
         columns = row.find_all('td')
-        print(f"{columns}")
+
         if columns:
             name = columns[0].text.strip()
-
-            rows.append({'Name' : name})
+            age = columns[1].text.strip()
+            team = columns[2].text.strip()
+            pos = columns[3].text.strip()
+            gp = columns[4].text.strip()
+            goals = columns[5].text.strip()
+            assists = columns[6].text.strip()
+            points = columns[7].text.strip()
+            rows.append({'Name' : name, 'Age' :age, 'Team' : team, 'Pos' : pos, 'GP' : gp, 'Goals' : goals, 'Assists' : assists, 'Points' : points})
 
     df = pd.DataFrame(rows)
 
     print("Done scraping!")
+
+    return df
